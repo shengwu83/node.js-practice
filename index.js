8e819070779e76e74369677bd1948141d1d27676
ShengWu <alexwu.hchs@gmail.com>
2014-05-16 16:04:50 +0800

    圖片上傳功能

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
 var handle = {};
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
+handle["/"] = requestHandlers.post;
+handle["/postText"] = requestHandlers.postText;
+handle["/post"] = requestHandlers.post;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 
 server.start(router.route , handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,4 +1,9 @@
-function start(response){
+var querystring = require("querystring"),
+fs = require("fs"),
+formidable = require("formidable");
+
+
+function post(response){
 	console.log("request for 'start' called");
 
 	var body = '<html>'+
@@ -7,23 +12,71 @@ function start(response){
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function postText(response){
+	console.log("request for 'start' called");
+
+	var body = '<html>'+
+	'<head>'+
+	'<meta http-equiv="Content-Type" content="text/html; '+
+	'charset=UTF-8" />'+
+	'</head>'+
+	'<body>'+
+	'<form action="/upload" method="post">'+
+	'<textarea name="text" rows="20" cols="60" ></textarea>'+
+	'<input type="submit" value="submit" />'+
+	'</form>'+
+	'</body>'+
+	'</html>';
 
 	response.writeHead(200,{"Content-Type":"text/html"});
 	response.write(body);
 	response.end();
 }
-function upload(response){
-	console.log("request for 'upload' called");
 
-	response.writeHead(200,{"Content-Type":"text/plain"});
-	response.write("this is 'upload' page.");
-	response.end();
+function upload(response,request){
+	console.log("request for 'upload' called");
+	var form = new formidable.IncomingForm();
+	console.log('about to parse');
+	form.parse(request, function(error, fields, files){
+		console.log('parsing done');
+		fs.renameSync(files.upload.path,"./tmp/test.gif");
+
+		response.writeHead(200,{"Content-Type":"text/html"});
+		response.write("received image:<br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	});
 }
-exports.start = start
+
+function show(response, req){
+	console.log("request for 'show' called");
+	fs.readFile("./tmp/test.gif","binary",function(error, file){
+		if(error){
+			response.writeHead(500,{"Content-Type":"text/plain"});
+			response.write(error+"\n");
+			response.end();
+		}else{
+			response.writeHead(200, {"Content-Type":"image/gif"});
+			response.write(file, "binary");
+			response.end();
+		}
+	});
+
+}
+exports.postText = postText
+exports.post = post
 exports.upload = upload
+exports.show = show
